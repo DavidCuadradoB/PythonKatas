8076bbaaeeb97f2144c22d1516a0e3365caff136
DavidCuadradoBallesteros <davidcuadradoballesteros@gmail.com>
2023-04-07 19:54:20 +0200

    Create function throw in Frame
    So the Frame is the one which has the responsibility to validate it.

--- bowling/Frame.py
+++ bowling/Frame.py
@@ -1,6 +1,11 @@
+class FrameFull(Exception):
+    """Raised when a new attempt is added but the frame is full."""
+    pass
+
+
 class Frame(object):
-    firstTry = 0
-    secondTry = 0
+    firstTry = -1
+    secondTry = -1
 
     def get_score(self):
         return self.firstTry + self.secondTry
@@ -10,6 +15,15 @@ class Frame(object):
 
     def save_second_try(self, value):
         self.secondTry = value
+
+    def throw(self, value):
+        if self.firstTry == -1:
+            self.save_first_try(value)
+        elif self.secondTry == -1 and not self.is_strike():
+            self.save_second_try(value)
+        else:
+            raise FrameFull('The frame is full')
+
 
     def is_spare(self):
         return self.get_score() == 10
--- bowling/test/FrameTest.py
+++ bowling/test/FrameTest.py
@@ -1,10 +1,13 @@
 import unittest
 
-from bowling.Frame import Frame
+from bowling.Frame import Frame, FrameFull
 
 
 class MyTestCase(unittest.TestCase):
     frame = Frame()
+
+    def setUp(self):
+        self.frame = Frame()
 
     def test_get_score_given_first_and_second_try_equals_should_be_sum_of_both(self):
         first_strike = 10
@@ -33,15 +36,35 @@ class MyTestCase(unittest.TestCase):
         is_spare = self.frame.is_spare()
         self.assertEqual(is_spare, False)
 
-    def test_is_stike_given_first_try_equals_10_should_return_true(self):
+    def test_is_strike_given_first_try_equals_10_should_return_true(self):
         self.frame.save_first_try(10)
         is_strike = self.frame.is_strike()
         self.assertEqual(is_strike, True)
 
-    def test_is_stike_given_first_try_less_than_10_should_return_false(self):
+    def test_is_strike_given_first_try_less_than_10_should_return_false(self):
         self.frame.save_first_try(1)
         is_strike = self.frame.is_strike()
         self.assertEqual(is_strike, False)
+
+    def test_throw__given_frame_without_tries__should_save_the_value_in_first_try(self):
+        value = 5
+        self.frame.throw(value)
+        self.assertEqual(self.frame.firstTry, value)
+
+    def test_throw__given_frame_with_one_try__should_save_the_value_in_second_try(self):
+        self.frame.save_first_try(1)
+        value = 5
+        self.frame.throw(value)
+        self.assertEqual(self.frame.secondTry, value)
+
+    def test_throw__given_frame_with_both_tries__should_throw_an_exception(self):
+        self.frame.save_first_try(1)
+        self.frame.save_second_try(3)
+        value = 5
+        with self.assertRaises(FrameFull) as context:
+            self.frame.throw(value)
+
+        self.assertTrue('The frame is full' in str(context.exception))
 
 
 if __name__ == '__main__':
